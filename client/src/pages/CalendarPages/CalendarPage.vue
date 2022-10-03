db9ae0c3b23bcada79d3ef951e8006d972f0faa0
Dem Leonid <ledemi@ttu.ee>
2022-10-03 17:14:52 +0300

    zero page for settings is made + sidebar

---
+++ client/src/pages/CalendarPages/CalendarPage.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { RolesEnum } from '@/models/IUser';
+import CalendarClientPage from '@/pages/CalendarPages/CalendarClientPage.vue';
+import CalendarMasterPage from '@/pages/CalendarPages/CalendarMasterPage.vue';
+import { useAuthStore } from '@/store/useAuth';
+import { storeToRefs } from 'pinia';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    data: () => ({
+        RolesEnum,
+    }),
+    setup: () => {
+        const auth = useAuthStore();
+        auth.hasAccess();
+
+        const { authUser } = storeToRefs(auth);
+
+        return { authUser };
+    },
+    components: { CalendarClientPage, CalendarMasterPage },
+});
+</script>
+<template>
+    <div>
+        <CalendarClientPage v-if="authUser.role === RolesEnum.CLIENT" />
+        <CalendarMasterPage v-else-if="authUser.role === RolesEnum.MASTER" />
+    </div>
+</template>
+
+<style scoped></style>
---
+++ client/src/pages/SettingPages/SettingClientPage.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { useAuthStore } from '@/store/useAuth';
+import { storeToRefs } from 'pinia';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    setup: () => {
+        const auth = useAuthStore();
+        auth.hasAccess();
+    },
+});
+</script>
+
+<template>
+    <div>
+        <h1 class="text-center">Settings for Client</h1>
+        <div>
+            <ul>
+                <li>Есть фото профиля</li>
+                <li>Может менять личные данные</li>
+                <li>Имя,Фамилия, возраст, Пол, почту</li>
+                <li>Будет блок со статистикой:</li>
+                <li>Посмотреть историю посещений</li>
+                <li>На скольких услугах побывал</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
