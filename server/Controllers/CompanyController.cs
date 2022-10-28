e6ea1d84fd7db9a31668d31323b0e20a2dac0896
Vladislav Suprun <vlsupr@ttu.ee>
2022-10-28 12:38:45 +0300

    Company basic controllers done

---
+++ server/Controllers/CompanyController.cs
@@ -0,0 +1,97 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using server.Db;
+using server.Models;
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController: ControllerBase
+    {
+        private readonly DataContext _context;
+        public CompanyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{companyId}")]
+        public ActionResult<Company> GetCompany(int companyId)
+        {
+            var company = _context.Companies!.Find(companyId);
+
+            if (company == null)
+            {
+                return NotFound("Provided company does not exist");
+            }
+
+            return Ok(company);
+        }
+        [HttpGet("/CompaniesCount")]
+        public ActionResult<int> GetCompaniesCount()
+        {
+            var company = _context.Companies!
+            .ToList();
+
+            return Ok(company.Count);
+        }
+        
+        [HttpPost]
+        public ActionResult<Company> CreateCompany([FromBody] Company company) {
+           var dbCompany = _context.Companies!.Find(company.Id);
+            if (dbCompany == null)
+            {
+                _context.Add(company);
+                _context.SaveChanges();
+
+                return CreatedAtAction(nameof(GetCompany), new { Id = company.Id }, company);
+            }
+             if(_context.Companies!.Any(x => x.companyName == company.companyName))
+            {
+                return BadRequest("Company already exists with the same name");
+            }
+            else
+            {
+                return Conflict("Company already exists with the same Id");
+            }
+        }
+
+        [HttpPut("{companyId}")]
+        public IActionResult UpdateCompany(int companyId, Company company)
+        {
+            if (companyId != company.Id)
+            {
+                return BadRequest();
+            }
+
+            if (!CompanyExists(companyId))
+            {
+                return NotFound();
+            }
+
+            _context.Entry(company).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+        
+        [HttpDelete("{companyId}")]
+        public ActionResult<Company> DeleteCompany(int companyId)
+        {
+            if (!CompanyExists(companyId))
+            {
+                return NotFound();
+            }
+
+            var company = _context.Companies!.Find(companyId);
+            _context.Companies!.Remove(company);
+            _context.SaveChanges();
+
+            return company;
+        }
+     
+        private bool CompanyExists(int id)
+        {
+            return _context.Companies!.Any(c => c.Id == id);
+        }
+    }
+}
