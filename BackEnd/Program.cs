0060fd9464f42fc0d7ee5153fd0148830eeb1bf6
Vladislav Suprun <vlsupr@ttu.ee>
2022-11-17 18:55:57 +0200

    added token to appsettings

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using server.Db;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,17 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<DataContext>(opts => opts.UseNpgsql(builder.Configuration.GetConnectionString("default")));
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder => {
-    builder
-    .SetIsOriginAllowed(_ => true)
-    .AllowCredentials()
-    .AllowAnyMethod()
-    .AllowAnyHeader();
-}));
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -34,6 +24,18 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
         };
     });
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder => {
+    builder
+    .SetIsOriginAllowed(_ => true)
+    .AllowCredentials()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+}));
+
 
 var app = builder.Build();
 
